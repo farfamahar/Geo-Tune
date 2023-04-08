3f39346d232e8a1cb9638209cacbd70bd19ff0f6
Farfama <farfama.hargaaya@gmail.com>
2023-04-09 01:43:58 +0900

    Update looping
    - Looping toggle added

--- main.js
+++ main.js
@@ -7,6 +7,10 @@
       // Get the coordinates element
       const coordinates = document.getElementById("coordinates");
       const note = document.getElementById("note");
+      // Initialize loop state
+      let loop = false;
+      let isPlaying = false;
+      let index = 0;
 
       // Function to play a single note
       const playNote = (digit) => {
@@ -51,38 +55,69 @@
         }, 500);
       };
 
-      // Function to play a melody based on an array of digits
-      const playMelody = (digits) => {
-        // Loop through each digit and play a note based on its value
-        digits.forEach((digit, index) => {
-          // Delay playing each note by 0.5 seconds
-          setTimeout(() => {
-            note.textContent = `${digit}`;
-            playNote(digit);
-          }, index * 200);
-        });
-      };
+// Function to play a melody based on an array of digits
+const playMelody = (digits) => {
+    index = 0;
+  
+    // Loop through each digit and play a note based on its value
+    const playNextNote = () => {
+      const digit = digits[index];
+      note.textContent = `${digit}`;
+      playNote(digit);
+  
+      index++;
+  
+      // If looping, reset the index to 0 when it reaches the end
+      if (loop && index >= digits.length) {
+        index = 0;
+      }
+  
+      // If not at the end of the melody, schedule the next note to play
+      if (index < digits.length || loop) {
+        setTimeout(playNextNote, 200);
+      } else {
+        // Reset playing state when melody is finished
+        isPlaying = false;
+      }
+    };
+  
+    // Start playing the melody
+    isPlaying = true;
+    playNextNote();
+  };
 
-      // Event listener for drum buttons
-      drumButtons.forEach((button) => {
-        button.addEventListener("click", () => {
-          // Get user's current position and display coordinates
-          if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-              const { latitude, longitude } = position.coords;
-              coordinates.textContent = `Latitude : ${latitude.toFixed(
-                8
-              )}, Longitude: ${longitude.toFixed(8)}`;
-              // Play a melody based on the latitude and longitude digits
-              const latitudeDigits = String(latitude.toFixed(8)).match(/\d/g);
-              const longitudeDigits = String(longitude.toFixed(8)).match(/\d/g);
-              const digits = [...latitudeDigits, ...longitudeDigits];
-              playMelody(digits);
-            });
-          } else {
-            // If geolocation is not supported, play a default melody
-            const digits = [1, 2, 3, 4, 5];
-            playMelody(digits);
-          }
+// Event listener for drum buttons
+drumButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      // Get user's current position and display coordinates
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const { latitude, longitude } = position.coords;
+          coordinates.textContent = `Latitude : ${latitude.toFixed(
+            8
+          )}, Longitude: ${longitude.toFixed(8)}`;
+          // Play a melody based on the latitude and longitude digits
+          const latitudeDigits = String(latitude.toFixed(8)).match(/\d/g);
+          const longitudeDigits = String(longitude.toFixed(8)).match(/\d/g);
+          const digits = [...latitudeDigits, ...longitudeDigits];
+          playMelody(digits);
         });
-      });
+      } else {
+        // Geolocation is not supported
+        alert("Geolocation is not supported by this browser.");
+      }
+    });
+  });
+
+// Event listener for loop button
+const loopToggle = document.getElementById("loop-toggle");
+loopToggle.addEventListener("click", () => {
+  // Toggle loop state
+  loop = !loop;
+  loopToggle.textContent = loop ? "Looping On" : "Looping Off";
+
+  // If melody is already playing, reset it to play from the beginning
+  if (isPlaying) {
+    index = 0;
+  }
+});
